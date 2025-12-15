644bef87c1deac25c4d9de1e4dc9e8fe2da7e8d7
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2025-12-15 23:39:08 +0530

    Add files via upload

---
+++ Arrays/greet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class greet {
+    public static void main(String[] args){
+        String str = "anmol";
+        greeting(str);
+    }
+    static void greeting(String name){
+        System.out.print("Hello "+ name +" how you do'in");
+    }
+}
