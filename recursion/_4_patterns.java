f6c1006f3b1aacaf06cebd73158049e0bddb3638
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2026-01-06 23:57:11 +0530

    Add files via upload

---
+++ recursion/_4_patterns.java
@@ -0,0 +1,29 @@
+public class _4_patterns {
+    public static void main(String[] args){
+        triangle2(4,0);
+    }
+    static void triangle2(int row , int col){
+        if(row == 0){
+            return;
+        }
+        if(col<row){
+            triangle2(row , col+1);
+            System.out.print("*");
+        }else{
+            triangle2(row-1,0);
+            System.out.println();
+        }
+    }
+    static void triangle(int row , int col){
+        if(row == 0){
+            return;
+        }
+        if(col<row){
+            System.out.print("*");
+            triangle(row , col+1);
+        }else{
+            System.out.println();
+            triangle(row-1,0);
+        }
+    }
+}
