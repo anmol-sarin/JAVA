48c167e90e67da4e5860bb669ca5456702140b05
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2026-02-08 23:54:54 +0530

    Add files via upload

---
+++ OOPS/abstract_classes/Daughter.java
@@ -0,0 +1,13 @@
+package com.abstract_classes;
+
+public class Daughter extends Parent{
+    @Override
+    void career(){
+        System.out.println("I want to become a teacher");
+    }
+    @Override
+    void partner(){
+        System.out.println("I love superman");
+
+    }
+}
