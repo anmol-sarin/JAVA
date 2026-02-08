e1b9cbc39291e8e2dde119d258f062c4097299e2
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2026-02-08 23:55:42 +0530

    Add files via upload

---
+++ OOPS/abstract_classes/Main.java
@@ -0,0 +1,9 @@
+package com.abstract_classes;
+import com.abstract_classes.Son;
+public class Main {
+    Son son1 = new Son(15);
+//    son1.career();
+
+    Daughter d1 = new Daughter();
+//    d1.partner();
+}
