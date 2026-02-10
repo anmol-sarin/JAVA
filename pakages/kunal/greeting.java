c84e110925768a3e9711f77eccadb986d4f999f7
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2026-02-10 23:59:36 +0530

    Add files via upload

---
+++ pakages/kunal/greeting.java
@@ -0,0 +1,10 @@
+package com.pakages.kunal;
+
+public class greeting {
+    public static void main(String[] args) {
+        message();
+    }
+    public static void message(){
+        System.out.println("Hello World !!");
+    }
+}
