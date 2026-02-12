1c7124133958c52957b7df164d3b035eca0b5db2
Anmol Sarin <131007544+anmol-sarin@users.noreply.github.com>
2026-02-12 23:51:56 +0530

    Add files via upload

---
+++ OOPS/Polymorphism/Main.java
@@ -0,0 +1,12 @@
+package com.polymorphism;
+
+public class Main {
+    public static void main(String[] args){
+        Shapes shape = new Shapes();
+        Circle circle = new Circle();
+        Square square = new Square();
+        Triangle triangle = new Triangle();
+
+        square.area();
+    }
+}
